c03a3ed410d12492c2ee6699dc8daa9a4f42c147
Eka Setiawan Saputra <mr.poetra22@gmail.com>
2023-06-23 11:11:13 +0700

    update

--- printer.go
+++ printer.go
@@ -6,7 +6,7 @@ import (
 )
 
 func Print(filename, printer string) error {
-	output, err := exec.Command("lpr", "-r", "-P", printer, "-o", "portrait", "-o", "media=684x396", filename).CombinedOutput()
+	output, err := exec.Command("lpr", "-r", "-P", printer, "-o", "portrait", "-o", "media=A4", filename).CombinedOutput()
 	log.Println(output)
 
 	return err
